cd7484876d8ea5c185c01ee4f9c1acf0fa1d5cad
Muhammad Hassan <mhassan.codes@gmail.com>
2025-10-26 11:12:48 +0500

    comment updated

--- lib/domain/usecases/convert_currency_usecase.dart
+++ lib/domain/usecases/convert_currency_usecase.dart
@@ -1,12 +1,11 @@
-// lib/domain/usecases/convert_currency_usecase.dart
 import 'package:courency_converter/data/repositories/currency_repository.dart';
 import '../../core/constants/currency_data.dart';
 import '../models/currecny_conversion.dart';
 
 class ConvertCurrencyUseCase {
-  final CurrencyRepository _repository; // ✅ store repository
+  final CurrencyRepository _repository; //  store repository
 
-  ConvertCurrencyUseCase(this._repository); // ✅ proper constructor
+  ConvertCurrencyUseCase(this._repository); //  proper constructor
 
   double execute(String fromCurrency, String toCurrency, double amount) {
     // Use repository to convert (better than using constants directly)
