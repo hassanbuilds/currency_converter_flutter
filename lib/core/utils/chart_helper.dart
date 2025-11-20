2b41a96e75111073ba217c2fe682347a54cc678c
Muhammad Hassan <mhassan.codes@gmail.com>
2025-11-20 06:10:30 +0500

    comment updated

--- lib/core/utils/chart_helper.dart
+++ lib/core/utils/chart_helper.dart
@@ -3,16 +3,18 @@ import 'package:courency_converter/data/local/shared_prefs_service.dart';
 class ChartHelper {
   final SharedPrefsService _prefsService = SharedPrefsService();
 
-  // ---------------- Helper functions ----------------
+  // Helper functions
   List<double> _toDoubleList(List<dynamic> list) {
-    return list.map((e) => e is double ? e : double.tryParse(e.toString()) ?? 0.0).toList();
+    return list
+        .map((e) => e is double ? e : double.tryParse(e.toString()) ?? 0.0)
+        .toList();
   }
 
   List<double> _invertRates(List<double> rates) {
     return rates.map((rate) => rate == 0 ? 0.0 : 1.0 / rate).toList();
   }
 
-  // ---------------- Public Methods ----------------
+  // Public Methods
   Future<List<double>> getChartData(
     String from,
     String to, {
@@ -21,18 +23,23 @@ class ChartHelper {
   }) async {
     final key = "${from}_$to";
 
-    List<double> history = _toDoubleList(await _prefsService.getDoubleList(key));
+    List<double> history = _toDoubleList(
+      await _prefsService.getDoubleList(key),
+    );
 
     if (history.isEmpty) {
       final reverseKey = "${to}_$from";
-      List<double> reverseHistory = _toDoubleList(await _prefsService.getDoubleList(reverseKey));
+      List<double> reverseHistory = _toDoubleList(
+        await _prefsService.getDoubleList(reverseKey),
+      );
 
       if (reverseHistory.isNotEmpty) {
         history = _invertRates(reverseHistory);
       } else {
-        final liveRate = rates.containsKey(to) && rates.containsKey(from)
-            ? rates[to]! / rates[from]!
-            : 0.0;
+        final liveRate =
+            rates.containsKey(to) && rates.containsKey(from)
+                ? rates[to]! / rates[from]!
+                : 0.0;
         history = [liveRate];
         await _prefsService.saveDoubleList(key, history);
       }
@@ -47,7 +54,9 @@ class ChartHelper {
 
   Future<void> saveRate(String from, String to, double rate) async {
     final key = "${from}_$to";
-    List<double> history = _toDoubleList(await _prefsService.getDoubleList(key));
+    List<double> history = _toDoubleList(
+      await _prefsService.getDoubleList(key),
+    );
     history.add(rate);
     if (history.length > 30) history.removeAt(0);
     await _prefsService.saveDoubleList(key, history);
