d1c0f99a586bfa114ec36ff428bd0f9c1e5fce8c
Muhammad Hassan <mhassan.codes@gmail.com>
2025-11-20 05:38:46 +0500

    Responsive LoadingIndicator added

--- lib/presentation/widgets/loading_indicator.dart
+++ lib/presentation/widgets/loading_indicator.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 class LoadingIndicator extends StatelessWidget {
-  const LoadingIndicator({super.key, this.size = 50.0});
+  const LoadingIndicator({super.key, this.size});
 
-  final double size;
+  final double? size;
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final bool isTablet = width > 600;
+
+    final double indicatorSize = size ?? (isTablet ? 80 : 50);
+
     return Center(
       child: SizedBox(
-        height: size,
-        width: size,
-        child: const CircularProgressIndicator(),
+        height: indicatorSize,
+        width: indicatorSize,
+        child: const CircularProgressIndicator(strokeWidth: 4),
       ),
     );
   }
